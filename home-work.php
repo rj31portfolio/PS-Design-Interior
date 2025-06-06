40314686b2b306b1f9a0ccbe9aa33122e9564975
rj31portfolio <rjrajuali31@gmail.com>
2025-06-06 23:17:48 +0530

    work section

---
+++ home-work.php
@@ -0,0 +1,136 @@
+ <!-- How We Work Start -->
+    <div class="how-we-work">
+        <div class="container">
+            <div class="row section-row align-items-center">
+                <div class="col-lg-6">
+                    <!-- Section Title Start -->
+                    <div class="section-title dark-section">
+                        <h3 class="wow fadeInUp">how we work</h3>
+                        <h2 class="text-anime-style-2" data-cursor="-opaque">From concept to completion in<span> our work</span></h2>
+                    </div>
+                    <!-- Section Title End -->
+                </div>
+
+                <div class="col-lg-6">
+                    <!-- Section Title Content Start -->
+                    <div class="section-title-content dark-section">
+                        <p class="wow fadeInUp" data-wow-delay="0.2s">Our comprehensive approach guides you through each phase of the design process, from initial brainstorming and conceptualization.</p>
+                    </div>
+                    <!-- Section Title Content End -->
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-lg-12">
+                    <!-- How We Work List Start -->
+                    <div class="how-we-work-list">
+                        <!-- How We Item Start -->
+                        <div class="how-we-work-item wow fadeInUp">
+                            <div class="icon-box">
+                                <img src="images/icon-how-we-work-1.svg" alt="">
+                            </div>
+                            <div class="how-we-work-content">
+                                <h3>01. initial consultation</h3>
+                                <p>We start with a one-on meeting to understand your vision preferences and requirement.</p>
+                            </div>
+                        </div>
+                        <!-- How We Item End -->
+
+                        <!-- How We Item Start -->
+                        <div class="how-we-work-item wow fadeInUp" data-wow-delay="0.2s">
+                            <div class="icon-box">
+                                <img src="images/icon-how-we-work-2.svg" alt="">
+                            </div>
+                            <div class="how-we-work-content">
+                                <h3>02. design planning</h3>
+                                <p>This involves selecting materials, and layouts, furnishings, as well as creating 3D renderings.</p>
+                            </div>
+                        </div>
+                        <!-- How We Item End -->
+
+                        <!-- How We Item Start -->
+                        <div class="how-we-work-item wow fadeInUp" data-wow-delay="0.4s">
+                            <div class="icon-box">
+                                <img src="images/icon-how-we-work-3.svg" alt="">
+                            </div>
+                            <div class="how-we-work-content">
+                                <h3>03. project execution</h3>
+                                <p>With the design plans in this place, we manage and coordinate all aspects of the projects.</p>
+                            </div>
+                        </div>
+                        <!-- How We Item End -->
+
+                        <!-- How We Item Start -->
+                        <div class="how-we-work-item wow fadeInUp" data-wow-delay="0.6s">
+                            <div class="icon-box">
+                                <img src="images/icon-how-we-work-4.svg" alt="">
+                            </div>
+                            <div class="how-we-work-content">
+                                <h3>04. final review</h3>
+                                <p>After completing project we conduct a thorough walkthrough with you to review the space.</p>
+                            </div>
+                        </div>
+                        <!-- How We Item End -->
+                    </div>
+                    <!-- How We Work List End -->
+
+                    <!-- How Work Company Slider Start -->
+                    <div class="how-work-company-slider">
+                        <div class="swiper">
+                            <div class="swiper-wrapper">
+                                <!-- Company Support Logo Start -->
+                                <div class="swiper-slide">
+                                    <div class="company-logo">
+                                        <img src="images/company-logo-1.svg" alt="">
+                                    </div>
+                                </div>
+                                <!-- Company Support Logo End -->
+
+                                <!-- Company Support Logo Start -->
+                                <div class="swiper-slide">
+                                    <div class="company-logo">
+                                        <img src="images/company-logo-2.svg" alt="">
+                                    </div>
+                                </div>
+                                <!-- Company Support Logo End -->
+
+                                <!-- Company Support Logo Start -->
+                                <div class="swiper-slide">
+                                    <div class="company-logo">
+                                        <img src="images/company-logo-3.svg" alt="">
+                                    </div>
+                                </div>
+                                <!-- Company Support Logo End -->
+                                
+                                <!-- Company Support Logo Start -->
+                                <div class="swiper-slide">
+                                    <div class="company-logo">
+                                        <img src="images/company-logo-4.svg" alt="">
+                                    </div>
+                                </div>
+                                <!-- Company Support Logo End -->
+                                
+                                <!-- Company Support Logo Start -->
+                                <div class="swiper-slide">
+                                    <div class="company-logo">
+                                        <img src="images/company-logo-5.svg" alt="">
+                                    </div>
+                                </div>
+                                <!-- Company Support Logo End -->
+                                
+                                <!-- Company Support Logo Start -->
+                                <div class="swiper-slide">
+                                    <div class="company-logo">
+                                        <img src="images/company-logo-1.svg" alt="">
+                                    </div>
+                                </div>
+                                <!-- Company Support Logo End -->
+                            </div>
+                        </div>
+                    </div>
+                    <!-- How Work Company Slider End -->
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- How We Work End -->
